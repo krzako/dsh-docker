35f29050f58623d03955b211662be2cc97e4c306
Krzako <krzako@gmail.com>
2026-09-20 14:21:27 +0200

    refactor(dsh): move PDF export addon into deepseek-harness

--- dockers/entrypoint.sh
+++ dockers/entrypoint.sh
@@ -77,8 +77,6 @@ server.listen(PUBLIC_PORT, PUBLIC_HOST, () => {
 });
 NODE
 
-/usr/local/bin/install-dsh-addons
-
 exec node "$DSH_SOURCE_DIR/apps/cli/lib/bin.js" web \
   --port 3081 \
   --trusted-host localhost:3080 \
